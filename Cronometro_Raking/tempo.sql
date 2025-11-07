f6d9d10fe5a94e8746810307781a642e53309c60
wenderson <wenderson.souza273127@gmail.com>
2025-11-07 12:23:05 -0300

    Raking

--- Cronometro_Raking/tempo.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS tempos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT NOT NULL,
-    tempo VARCHAR(20) NOT NULL,     -- tempo como string "HH:MM:SS" ou até "DDD:HH:MM:SS"
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
